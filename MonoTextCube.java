195d2c9dfe8c30f4759d406e0e6d594733b6d9af
farr16 <msamfarr@outlook.com>
2017-03-27 14:09:48 -0700

    Add text filtering
    Adds filtering of text to remove non-alphanumeric characters and
    converts the string to uppercase.

--- MonoTextCube.java
+++ MonoTextCube.java
@@ -1,17 +1,39 @@
 import java.util.Scanner;
 
+/**
+ * MonoTextCube
+ *
+ * @author Matthew Farr
+ * 
+ * This class defines a main method for a program which takes in user input from the console and
+ * generates an ASCII text art cube made from the text.
+ */
 public class MonoTextCube {
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) 
+	{
+		// Get text input
 		Scanner scanner = new Scanner(System.in);
-		
 		System.out.print("\nInput text to make into a text-cube: ");
-		
 		String input = scanner.nextLine();
 		
-		System.out.println("Input: " + input);
-		
+		// Filter the input text of whitespace and non alphanumeric characters
+		input = filterInput(input);
+		System.out.println("Filtered input: " + input);
+	}
+	
+	/**
+	 * filterInput
+	 * 
+	 * Method for filtering the input string of non alphanumeric characters. Used to make input
+	 * suitable for creating the monospace text cube with edges with a solid appearance.
+	 *
+	 * @param str: The string to be filtered
+	 *
+	 */
+	private static String filterInput (String str)
+	{
+		return str.replaceAll("[^A-Za-z0-9]", "").toUpperCase();
 	}
 	
 }
