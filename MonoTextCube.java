5edc737a77f7ebb0aa511062ef201784c147914e
farr16 <msamfarr@outlook.com>
2017-03-27 12:36:18 -0700

    Add MonoTextCube.java
    Adds the main program file to the project.
    
    Currently only asks for input and then echoes it to the console, to test
    basic I/O functionality.

---
+++ MonoTextCube.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MonoTextCube {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("\nInput text to make into a text-cube: ");
+		
+		String input = scanner.nextLine();
+		
+		System.out.println("Input: " + input);
+		
+	}
+	
+}
